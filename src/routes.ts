695493d9d34716d58537642321f89e20ba25b7b5
Eduardo Reche Lopes <eduardo.rechelopes@exxonmobil.com>
2021-06-23 20:33:01 -0300

    middleware to intercept calls

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import TagCreationController from './controllers/TagCreationController';
 import UserCreationController from './controllers/UserCreationController';
+import ensureAdmin from './middlewares/ensureAdmin';
 
 const router = Router();
 
 router.post('/users', new UserCreationController().handle);
-
-router.post('/tags', new TagCreationController().handle);
+router.post('/tags', ensureAdmin, new TagCreationController().handle);
 
 export default router;
