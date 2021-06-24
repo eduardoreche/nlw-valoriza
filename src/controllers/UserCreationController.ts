b14549ff6d2a01f65a6b5fffbfb523abc56d30e7
Eduardo Reche Lopes <eduardo.rechelopes@exxonmobil.com>
2021-06-24 18:50:13 -0300

    add password column to the user

--- src/controllers/UserCreationController.ts
+++ src/controllers/UserCreationController.ts
@@ -3,11 +3,11 @@ import UserCreationService from '../services/UserCreationService';
 
 class UserCreationController {
   async handle(request: Request, response: Response) {
-    const { name, email, admin } = request.body;
+    const { name, email, password, admin } = request.body;
 
     const service = new UserCreationService();
 
-    const user = await service.execute({ name, email, admin });
+    const user = await service.execute({ name, email, password, admin });
 
     return response.json(user);
   }
--- src/services/UserCreationService.ts
+++ src/services/UserCreationService.ts
@@ -1,14 +1,16 @@
+import { hash } from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
 import UsersRepositories from '../repositories/UsersRepositories';
 
 interface IUserCreationRequest {
   name: string;
   email: string;
+  password: string;
   admin?: boolean;
 }
 
 class UserCreationService {
-  async execute({ name, email, admin }: IUserCreationRequest) {
+  async execute({ name, email, password, admin }: IUserCreationRequest) {
     const usersRepository = getCustomRepository(UsersRepositories);
 
     if (!email) throw new Error('Email is invalid');
@@ -19,15 +21,18 @@ class UserCreationService {
 
     if (userAlreadyExists) throw new Error('User already exists');
 
+    const passwordHash = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
+      password: passwordHash,
       admin,
     });
 
     await usersRepository.save(user);
 
-    return user;
+    return { id: user.id, name, email, admin };
   }
 }
 
